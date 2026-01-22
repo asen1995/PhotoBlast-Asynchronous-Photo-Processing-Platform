ea2f01f002c7851f54b6118d9dede0d0c19fa6fe
Asen Nikolaev <asen.nikolaev95@abv.bg>
2026-01-22 02:42:19 +0200

    init empty project

---
+++ src/main/java/com/photoblast/PhotoBlastApplication.java
@@ -0,0 +1,13 @@
+package com.photoblast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoBlastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoBlastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/photoblast/PhotoBlastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoblast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoBlastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
